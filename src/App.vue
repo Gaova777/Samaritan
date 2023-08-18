4460d6b556a0b2c0d2cfa10801218b35fe26d5a8
Juan Diego Garzon Ovalle <gaova777@utp.edu.co>
2023-08-18 18:07:13 -0500

    project Samaritan

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <input type="file" @change="file = $event.target.files[0]" />
+    <h2 v-if="file">There is a file</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      file: null
+    }
+  },
+  watch: {
+    file (value) {
+      console.log('------------------- FILE -------------------')
+      console.log(value)
+      console.log()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  max-width: initial;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
